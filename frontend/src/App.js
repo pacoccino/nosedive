4c710dc0808722ef0c0e18ff9537c01297485d65
Pacien Boisson <pakokrew@gmail.com>
2020-10-07 17:24:08 +0200

    Basic page with material-ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Nosedive
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Container maxWidth="md">
+        <Router>
+          <Switch>
+            <Route path="*">
+              <div>hello</div>
+            </Route>
+          </Switch>
+        </Router>
+      </Container>
     </div>
   );
 }
